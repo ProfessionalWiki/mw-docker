cede1d468821b119f48d8961723e3a5e54d3b6de
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-01-07 18:01:45 +0100

    Separate PHP 7.4 modification from additions

--- test.sh
+++ test.sh
@@ -1,10 +1,32 @@
 #! /bin/bash
 
-if docker exec temp-mw curl -fsSL http://localhost/wiki/Special:Version | grep "Jeroen De Dauw" > /dev/null
-then
-    echo -e "\e[32mWiki works\e[0m"
-    exit 0
-else
-    echo -e "\e[31mWiki is broken\e[0m"
-    exit 1
-fi
+STATUS=0
+
+function echoGood {
+    echo -e "\e[32mSuccess: $1\e[0m"
+}
+
+function echoBad {
+    echo -e "\e[31mFail: $1\e[0m"
+}
+
+function testContains {
+    if echo "$1" | grep "$2" > /dev/null
+    then
+        echoGood "$3"
+    else
+        echoBad "$3"
+        STATUS=1
+    fi
+}
+
+specialVersion=`docker exec temp-mw curl -fsSL http://localhost/wiki/Special:Version`
+
+testContains "$specialVersion" "Jeroen De Dauw" "Can reach Special:Version"
+testContains "$specialVersion" "Vector" "Vector is listed on Special:Version"
+
+apiPhp=`docker exec temp-mw curl -fsSL http://localhost/w/api.php`
+
+testContains "$apiPhp" "/w/api.php?action=help&amp;modules=edit" "Can reach api.php"
+
+exit $STATUS
